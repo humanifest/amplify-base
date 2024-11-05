6775c14196aa02104d02869d7bcfae1e6dd306ba
Rory Dahl <rory.dahl@protonmail.com>
2024-11-05 12:37:35 -0800

    update user attributes

--- amplify/auth/userAttributes.ts
+++ amplify/auth/userAttributes.ts
@@ -1,8 +1,40 @@
-const userAttributes = {
-  givenName: { order: 1, required: true, mutable: true },
-  familyName: { order: 2, required: true, mutable: true },
-  email: { order: 3, required: true },
-  phoneNumber: { order: 4, required: false },
+//  NOTE: https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-settings-attributes.html
+
+type AttributeObject = {
+  [key: string]: {
+    order: number;
+    required: boolean;
+    mutable: boolean;
+  };
 };
 
-export default userAttributes;
+const immutable = ["email"]; // List of immutable attributes
+const required = ["given_name", "family_name", "email"];
+
+function createdAttributes(attributes: string[]): AttributeObject {
+  return attributes.reduce((result, attribute, index) => {
+    result[attribute] = {
+      order: index + 1,
+      required: required.includes(attribute),
+      mutable: !immutable.includes(attribute), // Set mutable to false if in immutable array
+    };
+    return result;
+  }, {} as AttributeObject);
+}
+
+export const attributes = [
+  "given_name",
+  "nickname",
+  "middle_name",
+  "family_name",
+  "email",
+  "phone_number",
+  "profile",
+  "picture",
+  "website",
+  "gender",
+  "birthdate",
+  "address",
+];
+
+export default createdAttributes(attributes);

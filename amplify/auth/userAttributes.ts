b919d6c9b2fdba6dd6bbb8c1106e72d654ab7121
Rory Dahl <rory.dahl@protonmail.com>
2024-11-05 13:44:04 -0800

    revert

--- amplify/auth/userAttributes.ts
+++ amplify/auth/userAttributes.ts
@@ -1,42 +1,29 @@
-//  NOTE: https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-settings-attributes.html
-
-import { UserAttributes } from "aws-cdk-lib/aws-iam";
-
-type AttributeObject = {
-  [key: string]: {
-    order: number;
-    required: boolean;
-    mutable: boolean;
-  };
+const userAttributes = {
+  givenName: { order: 1, required: true, mutable: true },
+  familyName: { order: 2, required: true, mutable: true },
+  email: { order: 3, required: true },
+  phoneNumber: { order: 4, required: false },
 };
 
-const immutable = ["email"]; // List of immutable attributes
-const required = ["givenName", "familyName", "email"];
-
-function createdAttributes(attributes: string[]): AttributeObject {
-  return attributes.reduce((result, attribute, index) => {
-    result[attribute] = {
-      order: index + 1,
-      required: required.includes(attribute),
-      mutable: !immutable.includes(attribute), // Set mutable to false if in immutable array
-    };
-    return result;
-  }, {} as AttributeObject);
-}
-
-export const attributes = [
-  "givenName",
+export const extendedUserAttributeOptions = [
+  "name",
+  "family_name",
+  "given_name",
+  "middle_name",
   "nickname",
-  "middleName",
-  "familyName",
-  "email",
-  "phone_number",
+  "preferred_username",
   "profile",
   "picture",
   "website",
   "gender",
   "birthdate",
+  "zoneinfo",
+  "locale",
+  "updated_at",
   "address",
+  "email",
+  "phone_number",
+  "sub",
 ];
 
-export default createdAttributes(attributes) as Partial<UserAttributes>;
+export default userAttributes;

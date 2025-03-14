bb041f5dbbf44987c542337b47a6054cfaf63b2d
Rory Dahl <rory.dahl@protonmail.com>
2025-03-13 19:32:28 -0700

    use method

--- amplify/cicd/cognito-setup.mjs
+++ amplify/cicd/cognito-setup.mjs
@@ -1,38 +1,35 @@
 import { Amplify } from "aws-amplify";
 import amplifyOutputs from "../../amplify_outputs.json" assert { type: "json" };
 import { AtgCognito } from "./atg-cognito.mjs";
-import testCredentialsData from "./.atg.tester.json" assert { type: "json" };
 
 // Configure Amplify with the outputs
 Amplify.configure(amplifyOutputs);
 
 const atgCognito = new AtgCognito(amplifyOutputs);
-const testCredentials = testCredentialsData.default;
 
 async function setupCognitoUsers() {
-  if (testCredentials.length != 0) {
+  // ✅ Use `readCredentialsFile` from AtgCognito
+  const testCredentials = await atgCognito.readCredentialsFile(
+    new URL("./.atg.tester.json", import.meta.url)
+  );
+
+  if (testCredentials.length > 0) {
     for (const cred of testCredentials) {
-      console.log("✅", testCredentialsData);
-      // Just in case. You can safely ignore error here
-      // TODO - fail silently? Some failures (e.g. token expired) suggest not.
+      const { username, password, givenName, familyName } = cred;
+      console.log("✅", JSON.stringify(cred));
 
       try {
-        await atgCognito.deleteUser(cred.username);
-        await atgCognito.createUser(
-          cred.username,
-          cred.password,
-          cred.givenName,
-          cred.familyName
-        );
-        await atgCognito.verifyUserEmail(cred.username);
-        await atgCognito.confirmUser(cred.username);
+        await atgCognito.deleteUser(username);
+        await atgCognito.createUser(username, password, givenName, familyName);
+        await atgCognito.verifyUserEmail(username);
+        await atgCognito.confirmUser(username);
       } catch (error) {
         console.error(`❌ Error creating or confirming user: ${error}`);
         process.exit(1);
       }
     }
   } else {
-    console.warn("No test users found!");
+    console.warn("⚠️ No test users found!");
   }
 }
 
